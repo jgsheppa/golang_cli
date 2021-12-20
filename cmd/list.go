320dd1aa1e0441fbfe871effd46dcd9857cb64cc
jgsheppa <james.sheppard@avisio.ai>
2021-12-20 01:15:55 +0100

    Create head and tail flags for list cmd

--- cmd/list.go
+++ cmd/list.go
@@ -19,6 +19,8 @@ import (
 var (
 	doneOpt bool
 	allOpt bool
+	tail bool
+	head bool
 )
 
 // listCmd represents the list command
@@ -39,31 +41,36 @@ func listRun(cmd *cobra.Command, args []string) {
 	sort.Sort(todo.ByPri(items))
 	
 	w := tabwriter.NewWriter(os.Stdout, 3, 0, 1, ' ', 0)
-	for _, item := range items {
-		if allOpt || item.Done == doneOpt {
-			fmt.Fprintln(w, item.Label()+"\t"+item.PrettyDone()+"\t"+item.PrettyPrint()+"\t"+item.Text+"\t")
+
+	switch {
+	case head:
+		for i := 1; i < 6; i++ {
+			if allOpt || items[i].Done == doneOpt {
+				fmt.Fprintln(w, items[i].Label()+"\t"+items[i].PrettyDone()+"\t"+items[i].PrettyPrint()+"\t"+items[i].Text+"\t")
+			}
 		}
+	case tail:
+		for i := len(items) - 5; i < len(items); i++ {
+			if allOpt || items[i].Done == doneOpt {
+				fmt.Fprintln(w, items[i].Label()+"\t"+items[i].PrettyDone()+"\t"+items[i].PrettyPrint()+"\t"+items[i].Text+"\t")
+			}
+		}
+	default:
+		for _, item := range items {
+			if allOpt || item.Done == doneOpt {
+				fmt.Fprintln(w, item.Label()+"\t"+item.PrettyDone()+"\t"+item.PrettyPrint()+"\t"+item.Text+"\t")
+			}
+	} 
 
 	}
-
-
 
 	w.Flush()
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
-	listCmd.Flags().BoolVar(&doneOpt, "done", false, "Show 'Done' Todos")
-	listCmd.Flags().BoolVar(&allOpt, "all", false, "Show all Todos")
-
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	listCmd.Flags().BoolVar(&doneOpt, "done", false, "Show 'Done' tasks")
+	listCmd.Flags().BoolVar(&allOpt, "all", false, "Show all tasks")
+	listCmd.Flags().BoolVar(&head, "head", false, "Show first five tasks")
+	listCmd.Flags().BoolVar(&tail, "tail", false, "Show last five tasks")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -46,11 +46,12 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.golang_cli.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/golang_cli.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	viper.BindPFlag("datafile", rootCmd.PersistentFlags().Lookup("datafile"))
 }
 
 // Read in config file and ENV variables if set.
